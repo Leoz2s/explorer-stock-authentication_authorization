5f50ac2b7aed337a13254d4f8344794277a06336
Leonardo <leonardosilvasantosss22@gmail.com>
2024-04-03 21:18:15 -0300

    Feat: Routes availability by Authorization

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -2,15 +2,32 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
+import { AdminRoutes } from './admin.routes';
+import { SaleRoutes } from './sale.routes';
+import { CustomerRoutes } from './customer.routes';
+import { USER_ROLE } from '../utils/roles';
+
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute() {
+    switch(user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      default: 
+        return <CustomerRoutes />;
+    };
+  };
+
   return (
     <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
